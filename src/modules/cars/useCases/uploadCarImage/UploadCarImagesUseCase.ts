6bb43304e8c422083dfaa75b79841e7f02d35b28
Carlos Henrique <carlos_6282010@hotmail.com>
2021-07-12 18:51:56 -0300

    amazon s3 upload

--- src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import CarsImageRepository from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/IStorageProvider';
 import { deleteFile } from '@utils/file';
 
 interface IRequest {
@@ -13,20 +14,24 @@ class UploadCarImagesUseCase {
   constructor(
     @inject('CarsImagesRepository')
     private carsImagesRepository: CarsImageRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
-    const carImages = await this.carsImagesRepository.findByCarId(car_id)
+    const carImages = await this.carsImagesRepository.findByCarId(car_id);
 
     if (carImages.length) {
       carImages.forEach(async image => {
         await deleteFile(`./tmp/cars/${image.image_name}`);
-        await this.carsImagesRepository.deleteByImageName(image.image_name)
-      })
+        await this.carsImagesRepository.deleteByImageName(image.image_name);
+        await this.storageProvider.delete(image.image_name, 'cars');
+      });
     }
 
-    images_name.forEach(image => {
-      this.carsImagesRepository.create(car_id, image);
+    images_name.forEach(async image => {
+      await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, 'cars');
     });
   }
 }
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,8 +3,21 @@ import { container } from 'tsyringe';
 import './MailProvider';
 import { IDateProvider } from './DateProvider/IDateProvider';
 import DayJsDateProvider from './DateProvider/implementations/DayJsDateProvider';
+import LocalStorageProvider from './StorageProvider/implementations/LocalStorageProvider';
+import S3StorageProvider from './StorageProvider/implementations/S3StorageProvider';
+import IStorageProvider from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
   'DayJsDateProvider',
   DayJsDateProvider,
+);
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  diskStorage[process.env.DISK],
 );
