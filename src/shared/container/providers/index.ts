7a0b74c063065b99ccb6c1b50798fc4b8ffc132c
Carlos Henrique <carlos_6282010@hotmail.com>
2021-07-13 11:38:26 -0300

    sending email by SES

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,23 +1,3 @@
-import { container } from 'tsyringe';
-
+import './DateProvider';
 import './MailProvider';
-import { IDateProvider } from './DateProvider/IDateProvider';
-import DayJsDateProvider from './DateProvider/implementations/DayJsDateProvider';
-import LocalStorageProvider from './StorageProvider/implementations/LocalStorageProvider';
-import S3StorageProvider from './StorageProvider/implementations/S3StorageProvider';
-import IStorageProvider from './StorageProvider/IStorageProvider';
-
-container.registerSingleton<IDateProvider>(
-  'DayJsDateProvider',
-  DayJsDateProvider,
-);
-
-const diskStorage = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  diskStorage[process.env.DISK],
-);
+import './StorageProvider';
